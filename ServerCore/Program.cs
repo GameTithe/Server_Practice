877336d34b645a133ff7efd01e5d9bd32a9a60b2
jangjaebum <wkdwobum@gmail.com>
2022-07-26 22:53:56 +0900

    Memory Barrier #1

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -3,33 +3,52 @@
 
 class Progam
 {
-    
+    //메모리 배리어
+    // A) 코드 재배치 억제
+    // B) 가시성
+
+    static volatile int x = 0;
+    static volatile int y = 0;
+    static volatile int r1 = 0;
+    static volatile int r2 = 0;
+
+    static void Thread_1()
+    {
+        y = 1;
+
+        // ------------------------
+        Thread.MemoryBarrier();
+
+        r1 = x;
+    }
+
+    static void Thread_2()
+    {
+        x = 1;
+        // ------------------------
+        Thread.MemoryBarrier();
+        r2 = y;
+    }
+
     static void Main(string[] args)
     {
-        int[,] arr = new int[10000, 10000];
-
+        int count = 0;
+        while(true)
         {
-            long now = DateTime.Now.Ticks;
+            count++;
+            x = y = r1 = r2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
 
-             for(int y = 0; y < 10000; y++)
-             {
-                for (int x = 0; x < 10000; x++)
-                    arr[y, x] = 1;
-             }
-            long end = DateTime.Now.Ticks;
-            Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            if (r1 == 0 && r2 == 0)
+                break;
         }
 
-        {
-            long now = DateTime.Now.Ticks;
-
-            for (int y = 0; y < 10000; y++)
-            {
-                for (int x = 0; x < 10000; x++)
-                    arr[x, y] = 1;
-            }
-            long end = DateTime.Now.Ticks;
-            Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
-        }
+        Console.WriteLine($"{count} 번 만에 빠져나옴");
     }
 }
