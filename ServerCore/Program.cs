18559fff162e6bf49c713d8a2359d13e82f56169
jangjaebum <wkdwobum@gmail.com>
2022-07-27 00:11:21 +0900

    Memory Barrior #2

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -3,52 +3,26 @@
 
 class Progam
 {
-    //메모리 배리어
-    // A) 코드 재배치 억제
-    // B) 가시성
-
-    static volatile int x = 0;
-    static volatile int y = 0;
-    static volatile int r1 = 0;
-    static volatile int r2 = 0;
-
-    static void Thread_1()
+    int _answer;
+    bool _complete;
+     
+    void A()
     {
-        y = 1;
-
-        // ------------------------
+        _answer = 123;
         Thread.MemoryBarrier();
-
-        r1 = x;
-    }
-
-    static void Thread_2()
+        _complete = true;
+        Thread.MemoryBarrier();
+    }     
+    void B()
     {
-        x = 1;
-        // ------------------------
         Thread.MemoryBarrier();
-        r2 = y;
+        if(_complete)
+        {
+            Thread.MemoryBarrier();
+            Console.WriteLine(_answer);
+        }
     }
-
     static void Main(string[] args)
     {
-        int count = 0;
-        while(true)
-        {
-            count++;
-            x = y = r1 = r2 = 0;
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            if (r1 == 0 && r2 == 0)
-                break;
-        }
-
-        Console.WriteLine($"{count} 번 만에 빠져나옴");
     }
 }
