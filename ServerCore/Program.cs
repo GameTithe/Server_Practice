13c53962384ee4e9579325ad77bb471f37d95b55
jangjaebum <wkdwobum@gmail.com>
2022-07-26 22:24:17 +0900

    Cash

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -3,35 +3,33 @@
 
 class Progam
 {
-    volatile static bool _stop = false;
-
-    static void ThreadMain()
-    {
-        Console.WriteLine("쓰레드 시작!");
-
-        while(_stop == false)
-        {
-            //누군가가 stop 신호를 해주기를 기다린다
-        }
-
-        Console.WriteLine("쓰레드 종료!");
-    }   
-
+    
     static void Main(string[] args)
     {
-        Task t = new Task(ThreadMain);
-        t.Start();
+        int[,] arr = new int[10000, 10000];
 
-        Thread.Sleep(1000);
+        {
+            long now = DateTime.Now.Ticks;
 
-        _stop = true;
+             for(int y = 0; y < 10000; y++)
+             {
+                for (int x = 0; x < 10000; x++)
+                    arr[y, x] = 1;
+             }
+            long end = DateTime.Now.Ticks;
+            Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+        }
 
-        Console.WriteLine("Stop 호출");
-        Console.WriteLine("종료 대기 중");
+        {
+            long now = DateTime.Now.Ticks;
 
-        //Thread q ;  q.Join()  같은 의미
-        t.Wait();
-
-        Console.WriteLine("종료 성공");
+            for (int y = 0; y < 10000; y++)
+            {
+                for (int x = 0; x < 10000; x++)
+                    arr[x, y] = 1;
+            }
+            long end = DateTime.Now.Ticks;
+            Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
+        }
     }
 }
