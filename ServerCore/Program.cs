fabacdc6c790e4b4926359d5ef48ae41fe44b6c1
jangjaebum <wkdwobum@gmail.com>
2022-08-08 15:02:20 +0900

    Context Switching

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -16,6 +16,12 @@
             int desire = 1;
             if( Interlocked.CompareExchange(ref _locked, desire , expected) == expected)
                 break;
+
+            //쉬다올게!
+
+            //Thread.Sleep(1); // 무조건 휴식 => 무조건 1ms정도 쉬고 싶어요
+            //Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가
+            Thread.Yield(); // 관대한 양보 => 관대하게 양보할 테니, 지금 실행이 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 자신에게 시간을 사용
         }
     }
 
