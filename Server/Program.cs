36c3b48c2e361cca70d66fa92da6a2df1ee6ebdb
jangjaebum <wkdwobum@gmail.com>
2022-08-19 18:55:23 +0900

    Serialization

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,82 +2,34 @@
 using System.Net.Sockets;
 using System.Text;
 using ServerCore;
-class Packet
+
+namespace Server
 {
-    public ushort size;
-    public ushort packetId;
-
-}
-class GameSession : PacketSession   
-{
-    public override void OnConnected(EndPoint endPoint)
+    class Program
     {
-        Console.WriteLine($"OnConnected : {endPoint}");
 
-        //Packet packet = new Packet() { size = 4, packetId = 7 };
+        static Listener _listener = new Listener();
 
-
-        //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-
-        //byte[] buffer = BitConverter.GetBytes(packet.size);
-        //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-        //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-        //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
-
-        //ArraySegment<byte> sendBuffer = SendBufferHelper.Close(buffer.Length + buffer2.Length);
-        //Send(sendBuffer);
-
-        Thread.Sleep(5000);
-        Disconnect();
-
-
-    }
-
-    public override void OnRecvPacket(ArraySegment<byte> buffer)
-    {
-        ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-        ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
-
-        Console.WriteLine($"RecvPacketId  : {id} , Size = {size}");
-    }
-
-    public override void OnDisconnected(EndPoint endPoint)
-    {
-        Console.WriteLine($"OnDisconnecrted : {endPoint}");
-    }
-
-    
-    public override void OnSend(int numOfBytes)
-    {
-        Console.WriteLine($"Transfferd bytes : {numOfBytes}");
-
-    }
-}
-
-class Program
-{
-
-    static Listener _listener = new Listener();
-
-    static void Main(string[] Args)
-    {
-        // DNS (Domain Name System)
-        string host = Dns.GetHostName();
-        IPHostEntry ipHost = Dns.GetHostEntry(host);
-        IPAddress ipAddr = ipHost.AddressList[0];
-        IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
-
-        // 문지기
-
-        _listener.Init(endPoint, () => { return new GameSession(); });
-
-        Console.WriteLine("Listening...");
-
-        while (true)
+        static void Main(string[] Args)
         {
-            ;
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 문지기
+
+            _listener.Init(endPoint, () => { return new ClientSession(); });
+
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                ;
+            }
+
+
         }
-
-
     }
 }
