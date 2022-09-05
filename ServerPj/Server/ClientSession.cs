b0f31be58f8dfeddd0643a310fccc2e8cf3fecb4
jangjaebum <wkdwobum@gmail.com>
2022-09-05 16:00:23 +0900

    Serialization..ing #1

---
+++ ServerPj/Server/ClientSession.cs
@@ -0,0 +1,109 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerCore;
+
+namespace Server
+{
+    class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+    }
+
+    class PlayerInfoOk : Packet
+    {
+        public int hp;
+        public int attack;
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2,
+    }
+
+    class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            //Packet packet = new Packet() { size = 100, packetId = 10 };
+
+            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            //byte[] buffer = BitConverter.GetBytes(packet.size);
+            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
+            //Send(sendBuff);
+
+            Thread.Sleep(5000);
+            Disconnect();
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            ushort count = 0;
+
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            count += 2;
+
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            switch((PacketID)id)
+            {
+                case PacketID.PlayerInfoReq:
+                {
+                    long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
+                    count += 8;
+                    Console.WriteLine($"PlayerInfoReq : {playerId}");
+                }
+                break;
+            }
+
+            Console.WriteLine($"RecvPacket ID : {id} , Size = {size}");
+        }
+
+        public override void OnDisConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisConnected : {endPoint}");
+        }
+
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes : {numOfBytes}");
+        }
+    }
+
+    class Program
+    {
+        static Listener _listener = new Listener();
+
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAdr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAdr, 7777);
+
+            _listener.Init(endPoint, () => { return new ClientSession(); });
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                ;
+            }
+        }
+    }
+
+}
---
+++ ServerPj/ServerPj/ServerSession.cs
@@ -0,0 +1,112 @@
+﻿using ServerCore;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+    }
+
+    class PlayerInfoOk : Packet
+    {
+        public int hp;
+        public int attack;
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2,
+    }
+
+    class ServerSession : Session
+    {   
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            PlayerInfoReq packet = new PlayerInfoReq() { size = 4, packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001};
+
+            //보낸다
+            //for (int i = 0; i < 5; i++)
+            {
+                ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+
+                ushort count = 0; 
+                bool success = true;
+                
+                //success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count),packet.size);
+                count += 2;
+
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count),packet.packetId);
+                count += 2;
+
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count , s.Count - count),packet.playerId);
+                count += 8;
+
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+
+                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
+
+                if(success)
+                    Send(sendBuff);
+            }
+        }
+
+        public override void OnDisConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisConnected : {endPoint}");
+        }
+
+        public override int OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, 0, buffer.Count);
+
+            Console.WriteLine($"[From Server] {recvData}");
+
+            return buffer.Count;
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes : {numOfBytes}");
+        }
+    }
+
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            string name = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(name);
+            IPAddress ipAdr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAdr, 7777);
+
+            Connector connector = new Connector();
+            connector.Connect(endPoint, () => { return new ServerSession(); });
+
+            while (true)
+            {
+                try
+                {
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+        }
+    }
+}
