c28b2153d6c6d8e8223be8c37196d9ea67c45574
jangjaebum <wkdwobum@gmail.com>
2022-11-08 00:39:58 +0900

    PacketGenerator #1

--- ServerPj/DummyClient/ServerSession.cs
+++ ServerPj/DummyClient/ServerSession.cs
@@ -6,16 +6,7 @@ using System.Text;
 
 namespace DummyClient
 {
-    public abstract class Packet
-    {
-        public ushort size;
-        public ushort packetId;
-
-        public abstract ArraySegment<byte> Write();
-        public abstract void Read(ArraySegment<byte> s);
-    }
-    
-    class PlayerInfoReq : Packet
+    class PlayerInfoReq
     {
         public long playerId;
         public string name;
@@ -55,13 +46,9 @@ namespace DummyClient
         }
         public List<SkillInfo> skills = new List<SkillInfo>();
 
-        public PlayerInfoReq()
-        {
-
-            this.packetId = (ushort)PacketID.PlayerInfoReq;
-        }
+    
           
-        public override void Read(ArraySegment<byte> segment)
+        public  void Read(ArraySegment<byte> segment)
         {
             ushort count = 0;
 
@@ -98,7 +85,7 @@ namespace DummyClient
             }
         }
 
-        public override ArraySegment<byte> Write()
+        public ArraySegment<byte> Write()
         { 
             ArraySegment<byte> segment = SendBufferHelper.Open(4096);
 
@@ -109,7 +96,7 @@ namespace DummyClient
 
             count += sizeof(ushort);
 
-            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.packetId);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)PacketID.PlayerInfoReq);
             count += sizeof(ushort);
 
             success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.playerId);
@@ -130,8 +117,6 @@ namespace DummyClient
             foreach(SkillInfo skill in skills)
                 success &= skill.Write(s, ref count);
 
-
-            this.size = count;
             success &= BitConverter.TryWriteBytes(s, count);
 
 
