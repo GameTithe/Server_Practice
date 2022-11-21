7d8dc3631fa943c9de055a8552d0f3aaa0db6f59
jangjaebum <wkdwobum@gmail.com>
2022-11-21 22:46:07 +0900

    #RESET# SocketPrograming

---
+++ ServerP/ServerCore/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DNS (Domain Name System)
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(hostName);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket socket = new Socket(endPoint.AddressFamily , SocketType.Stream , ProtocolType.Tcp);
+
+            try
+            {
+                socket.Bind(endPoint);
+
+                socket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    Socket clientSocket = socket.Accept();
+
+                    //receive
+                    byte[] recvBytes = new byte[1024];
+                    int recvLen = clientSocket.Receive(recvBytes);
+                    string recvBuffer = Encoding.UTF8.GetString(recvBytes, 0, recvLen);
+
+                    Console.WriteLine($"[From Client] : {recvBuffer}");
+
+                    //send
+                    byte[] sendBytes = Encoding.UTF8.GetBytes($"Welecome MMORPG SERVER!");
+                    int sendLen = clientSocket.Send(sendBytes);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+              
+        }
+    }
+}
---
+++ ServerP/ServerP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHost =  Dns.GetHostEntry(hostName);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+           
+            try
+            {
+                Console.WriteLine("Connecting...");
+
+                socket.Connect(endPoint);
+
+                //send
+                byte[] sendByts = Encoding.UTF8.GetBytes("Hello World");
+                int sendLen = socket.Send(sendByts);
+
+
+                //recv
+                byte[] recvBytes = new byte[1024];
+                int recvLen = socket.Receive(recvBytes);
+                string recvBuffer = Encoding.UTF8.GetString(recvBytes, 0, recvLen);
+                Console.WriteLine($"[From Server] : {recvBuffer}");
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"{e.ToString}");
+            }
+
+        }
+    }
+}
